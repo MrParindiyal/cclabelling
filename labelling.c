8923cb39d571907fb915e224abcfde55d05b16b8
AP <136710760+MrParindiyal@users.noreply.github.com>
2025-04-11 09:37:07 +0000

    commenting

--- labelling.c
+++ labelling.c
@@ -17,27 +17,37 @@ int main(){
     
     int rows = sizeof(img)/sizeof(img[0]);
     int cols = sizeof(img[0])/sizeof(img[0][0]);
+
+    // assuming image is of bin format, we start from '2' to avoid confusion
     int curr = 2;
     
+    // start iterating the image
     for (int row = 0; row < rows; row++){
         for (int col = 0; col < cols; col++){
 
+            // pixel is not in 1st row or 1st col, and pixel is active (i.e. 1)
             if (col && row && img[row][col]){
             
+                // if both upper and left pixel are inactive, then create new label
                 if (!img[row - 1][col] && !img[row][col - 1]){
-                    img[row][col] = curr++;
+                    img[row][col] = curr++;  // and update the current label
                 }   
 
-                else {
+                else { // otherwise, atleast one of the upper or left pixel is active
+                    // copy upper label if it's active, otherwise copy left label
                     img[row][col] = img[row - 1][col] > 0 ? img[row - 1][col] : img[row][col - 1];
+
+                    // if left pixel is active and less than current label, we copy
                     img[row][col] = img[row][col - 1] < img[row][col] && img[row][col - 1] ? img[row][col - 1] : img[row][col];
                 }
             }
             
+            // if active pixel is in 1st row but not in 1st col
             else if (row == 0 && col > 0 && img[row][col]){
                 img[row][col] = !img[row][col - 1] ? curr++ : img[row][col - 1];
             }
 
+            // if pixel is in 1st col
             else if (col == 0 && img[row][col]){
                 img[row][col] = img[row - 1][col] ? img[row - 1][col] : curr++;
             }
